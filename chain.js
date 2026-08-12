85046bc22871fe631bced3b7785654bc1577bb2d
ensaf01 <mohammedensaf402@gmail.com>
2026-08-12 23:21:50 +0600

    object in array and multiple object have in array,here we lear access value,nested object and optional chaining

---
+++ chain.js
@@ -0,0 +1,9 @@
+// data access layer for chain data
+console.log("Chain data access layer initialized");
+const data=[{ id: 1, name: "Chain 1" }];
+
+console.log(data[0].name);// karon data akta arrray ,and array er moddhe object ase tai, data[0] dile first object ta pabo, then tar name property access korte hobe.array modhe 1 ta element ba object ase.
+
+const data2=[{ id: 1, name: "Chain 1" }, { id: 2, name: "Chain 2" }];
+
+console.log(data2[1].name);// karon data2 akta arrray ,and array er moddhe object ase tai data2[1] dile second object ta pabo, then tar name property access korte hobe.array modhe 2 ta element ba object ase.
