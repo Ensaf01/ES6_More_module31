35b156accaff849044bde705593112e496632945
ensaf01 <mohammedensaf402@gmail.com>
2026-08-16 12:32:48 +0600

    filter use for divsion by 10

--- practice.js
+++ practice.js
@@ -9,3 +9,10 @@ console.log(evenNumbers); // [2, 4, 6, 8, 10]
 const friends = ["Alice", "Bob", "Charlie", "David"];
 const upperCaseFriends=friends.map(x=>x.toUpperCase());
 console.log(upperCaseFriends); // ["ALICE", "BOB", "CHARLIE", "DAVID"]
+
+
+//task-3 divide all numbers by 10
+
+const numbers2 = [13, 20, 33, 40, 50];
+const divisionByTen=numbers2.filter(x=>x%10==0);
+console.log(divisionByTen); // [20, 40, 50]
