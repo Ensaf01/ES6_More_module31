8d5db43a1ccb7eb3cbe5c5ac460d33ebe67bdbc7
ensaf01 <mohammedensaf402@gmail.com>
2026-08-16 12:30:07 +0600

    convert odd to even use map and conver lowercse to upper case

---
+++ practice.js
@@ -0,0 +1,11 @@
+// task-1 : convert odd to even number
+
+const numbers = [1, 3, 5, 7, 9];
+const evenNumbers=numbers.map(x=>x+1);
+console.log(evenNumbers); // [2, 4, 6, 8, 10]
+
+// task-2 : convert all string to uppercase
+
+const friends = ["Alice", "Bob", "Charlie", "David"];
+const upperCaseFriends=friends.map(x=>x.toUpperCase());
+console.log(upperCaseFriends); // ["ALICE", "BOB", "CHARLIE", "DAVID"]
